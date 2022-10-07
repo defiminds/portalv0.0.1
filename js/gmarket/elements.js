0824d8caf910b380b368eb120a76034bc85793ce
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-07 14:06:39 -0300

    Create elements.js

---
+++ js/gmarket/elements.js
@@ -0,0 +1,56 @@
+function onelement1() {
+  var x = document.getElementById("chart1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement2() {
+  var x = document.getElementById("chartd1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement3() {
+  var x = document.getElementById("chartmk1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement4() {
+  var x = document.getElementById("chartXAUBTC1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement5() {
+  var x = document.getElementById("chartSP500");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement6() {
+  var x = document.getElementById("chartwhale1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement7() {
+  var x = document.getElementById("chartnews1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
