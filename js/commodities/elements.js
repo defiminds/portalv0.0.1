67c61c0508cec4f999cdfdaf2e5b1218c15712bc
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-07 16:27:31 -0300

    Create elements.js

---
+++ js/commodities/elements.js
@@ -0,0 +1,8 @@
+function onelement1() {
+  var x = document.getElementById("chart1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
