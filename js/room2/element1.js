c30d35af3dc0928033c9c537a3572cbde9094c3c
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-08 13:48:38 -0300

    Update element1.js

--- js/room2/element1.js
+++ js/room2/element1.js
@@ -14,3 +14,75 @@ function onelement2() {
     x.style.display = "none";
   }
 }
+function onelement3() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement4() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement5() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement6() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement7() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement8() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement9() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement10() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement11() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
