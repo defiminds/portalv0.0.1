e53a149bc2f6f28527638f4cb2e55722de80bb65
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-08 09:00:13 -0300

    Update element1.js

--- js/room2/element1.js
+++ js/room2/element1.js
@@ -6,3 +6,11 @@ function onelement1() {
     x.style.display = "none";
   }
 }
+function onelement2() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
