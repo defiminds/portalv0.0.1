74d9826374c872afa14d4d651a53ff9cc8fd3957
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-10 19:48:42 -0300

    Update elements.js

--- js/charts/elements.js
+++ js/charts/elements.js
@@ -6,3 +6,35 @@ function onelement1() {
     x.style.display = "none";
   }
 }
+function onelement2() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement3() {
+  var x = document.getElementById("chart3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement4() {
+  var x = document.getElementById("chart4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement5() {
+  var x = document.getElementById("chart5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
