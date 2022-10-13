5ad51f647974fbbfb527deb48d59a49705118d80
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-12 23:14:59 -0300

    Update elements.js

--- js/charts/elements.js
+++ js/charts/elements.js
@@ -38,3 +38,11 @@ function onelement5() {
     x.style.display = "none";
   }
 }
+function onelement6() {
+  var x = document.getElementById("chart6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
