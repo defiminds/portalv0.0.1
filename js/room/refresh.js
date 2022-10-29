18e19c1a65c6460cd4a9d349370af8dbdc8e94bd
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-29 11:30:32 -0200

    Update refresh.js

--- js/room/refresh.js
+++ js/room/refresh.js
@@ -1,39 +1,29 @@
-<script> 
 $(document).ready(function(){
 setInterval(function(){
       $("chart7").load(window.location.href + "chart7" );
 }, 3000);
 });
-</script>
 
-<script> 
 $(document).ready(function(){
 setInterval(function(){
       $("chart7").load(window.location.href + "chart8" );
 }, 3000);
 });
-</script>
 
-<script> 
 $(document).ready(function(){
 setInterval(function(){
       $("chart7").load(window.location.href + "chart9" );
 }, 3000);
 });
-</script>
 
-<script> 
 $(document).ready(function(){
 setInterval(function(){
       $("chart7").load(window.location.href + "chart10" );
 }, 3000);
 });
-</script>
 
-<script> 
 $(document).ready(function(){
 setInterval(function(){
       $("chart7").load(window.location.href + "chart11" );
 }, 3000);
 });
-</script>
