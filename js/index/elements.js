5cd4b3eaa7afc43129668d88813bebe85e722bc5
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2022-10-29 12:34:11 -0200

    Create elements.js

---
+++ js/index/elements.js
@@ -0,0 +1,96 @@
+function onelement1() {
+  var x = document.getElementById("chart1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement2() {
+  var x = document.getElementById("chart2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement3() {
+  var x = document.getElementById("chart3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement4() {
+  var x = document.getElementById("chart4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement5() {
+  var x = document.getElementById("chart5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement6() {
+  var x = document.getElementById("chart6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement7() {
+  var x = document.getElementById("chart7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement8() {
+  var x = document.getElementById("chart8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement9() {
+  var x = document.getElementById("chart9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement10() {
+  var x = document.getElementById("chart10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement11() {
+  var x = document.getElementById("chart11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function onelement12() {
+  var x = document.getElementById("chart12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
